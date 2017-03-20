3e5024e739905972fd8e9ce6dcac600db2e841a5
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-20 08:16:27 +0200

    Handle missing zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,7 +5,9 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
-  if zipcode.length < 5
+  if zipcode.nil?
+    zipcode = "0" * 5
+  elsif zipcode.length < 5
     zipcode = zipcode.rjust(5, '0')
   elsif zipcode.length > 5
     zipcode = zipcode.slice(0, 5)
