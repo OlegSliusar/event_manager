b970cfd86b3069f051430c534867867a77fe8b90
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-20 07:35:53 +0200

    Switch to CSV parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,8 @@
+require 'csv'
 puts "EventManager Initialized!"
 
-if File.exist? "event_attendees.csv"
-  lines = File.readlines "event_attendees.csv"
-  lines.each_with_index do |line, index|
-    next if index == 0
-    columns = line.split(',')
-    name = columns[2]
-    puts name
-  end
+contents = CSV.open "event_attendees.csv", headers: true
+contents.each do |row|
+  name = row[2]
+  puts name
 end
