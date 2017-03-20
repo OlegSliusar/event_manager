bc78caca01f5f6863a47d0d1a8d105fd2cdce412
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-20 07:18:22 +0200

    Print attendees' first names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,11 @@
 puts "EventManager Initialized!"
 
 if File.exist? "event_attendees.csv"
-  contents = File.read "event_attendees.csv"
-  puts contents
+  lines = File.readlines "event_attendees.csv"
+  lines.each_with_index do |line, index|
+    next if index == 0
+    columns = line.split(',')
+    name = columns[2]
+    puts name
+  end
 end
