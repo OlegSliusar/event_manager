5d3eb5de1975820a67708ca3fb33c3d200369f23
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-20 08:15:11 +0200

    Handle bad and good zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,5 +5,14 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+  if zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode.slice(0, 5)
+  end
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than 5 digits, truncate it to the first 5 digits
+  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
+
   puts "#{name} #{zipcode}"
 end
