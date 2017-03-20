deab389a81bf4f505d69c83f2023dc957c7f3742
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-20 10:31:41 +0200

    Show all legislators in a zip code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -6,10 +10,13 @@ end
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
